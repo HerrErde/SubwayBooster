cc7b0f7395ba61effb0b84868f66001119b4b90e
Herr Erde <Natodamm@hotmail.com>
2023-11-02 18:27:19 +0100

    fix fix

--- script/calender.py
+++ script/calender.py
@@ -4,11 +4,15 @@ with open("calendar_data.json", "r") as calendar_data_file:
     calendar_data = json.load(calendar_data_file)
     calendar_id = calendar_data.get("id")
 
-new_calendar_data = {
-    "lastSaved": None,
-    "calendarStates": {},
-    "completedCalendars": {calendar_id: True},
+new_data = {
+    "version": 3,
+    "data": {
+        "lastSaved": None,
+        "calendarStates": {},
+        "completedCalendars": {calendar_id: True},
+    },
 }
 
+
 with open("src/profile/calendars.json", "w") as calendar_file:
-    json.dump(new_calendar_data, calendar_file, indent=2)
+    json.dump(new_data, calendar_file, indent=2)
--- script/convert.py
+++ script/convert.py
@@ -1,46 +1,33 @@
 import os
 import json
 
+input_dir = "src/profile"
+output_dir = "temp"
 
-def process_json_file(input_file_name, output_file_name):
-    try:
-        # Read the input JSON file
-        with open(input_file_name, "r") as input_file:
-            data = json.load(input_file)
+os.makedirs(output_dir, exist_ok=True)
 
-        # Extract the content of the "data" field
-        data_content = json.dumps(data["data"], separators=(",", ":"))
-
-        # Create a new JSON object with the modified "data" field
-        new_data = {"version": data["version"], "data": data_content}
-
-        # Save the new JSON object to the output file
-        with open(output_file_name, "w") as output_file:
-            json.dump(new_data, output_file, indent=2)
-
-        print(f"File '{output_file_name}' created successfully.")
-    except (IOError, json.JSONDecodeError) as e:
-        print(f"Error processing '{input_file_name}': {str(e)}")
+# Get a list of all JSON files in the input folder
+json_files = [f for f in os.listdir(input_dir)]
 
 
-def main():
-    # Input folder containing JSON files
-    input_folder = "src/profile"
+def convert_json(input_file, output_file):
+    with open(input_file, "r") as input_file:
+        data = json.load(input_file)
 
-    # Output folder for the modified JSON files
-    output_folder = "temp"
+    # Remove spaces from the "data" field
+    data_content = json.dumps(data["data"]).replace(" ", "")
 
-    # Create the output folder if it doesn't exist
-    os.makedirs(output_folder, exist_ok=True)
+    # Create a new JSON object with the modified "data" field
+    new_data = {"version": data["version"], "data": data_content}
 
-    # Get a list of all JSON files in the input folder
-    json_files = [f for f in os.listdir(input_folder) if f.endswith(".json")]
+    # Save the new JSON object to the output file
+    with open(output_file, "w") as output:
+        json.dump(new_data, output, indent=2)
+        print(f"File '{output_file}' created successfully.")
 
-    # Process each JSON file in the input folder
-    for json_file in json_files:
-        # Construct the full path for input and output files
-        input_file_path = os.path.join(input_folder, json_file)
-        output_file_path = os.path.join(output_folder, json_file)
 
-        # Process the JSON file
-        process_json_file(input_file_path, output_file_path)
+for json_file in json_files:
+    input_file_path = os.path.join(input_dir, json_file)
+    output_file_path = os.path.join(output_dir, json_file)
+
+    convert_json(input_file_path, output_file_path)
