84994b6f0ce93378379beaddd473bd27ce3efea6
Herr Erde <Natodamm@hotmail.com>
2024-11-12 12:57:17 +0100

    remove zip android file path

--- script/zip.py
+++ script/zip.py
@@ -4,14 +4,9 @@ import os
 
 def zip_folder(folder_path="temp/data", output_zip="SubwayBooster.zip"):
     with zipfile.ZipFile(output_zip, "w", zipfile.ZIP_DEFLATED) as zipf:
-        # Define the root directory within the ZIP archive
-        root_directory = "Android/data/com.kiloo.subwaysurf/files/"
-
         # Copy src/version.json into the ZIP archive
         version_file_path = os.path.join("src", "version.json")
-        zipf.write(
-            version_file_path, arcname=os.path.join(root_directory, "version.json")
-        )
+        zipf.write(version_file_path, arcname=os.path.join("version.json"))
 
         # Iterate through the folder and include JSON files
         for root, _, files in os.walk(folder_path):
@@ -19,7 +14,7 @@ def zip_folder(folder_path="temp/data", output_zip="SubwayBooster.zip"):
                 file_path = os.path.join(root, file)
                 # Calculate the relative path and zip archive path
                 arcname = os.path.relpath(file_path, folder_path)
-                zip_arcname = os.path.join(root_directory, "profile", arcname)
+                zip_arcname = os.path.join("profile", arcname)
                 zipf.write(file_path, arcname=zip_arcname)
 
 
