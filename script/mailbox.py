410f38cd0cac1fced003af8b81ec53a4ef6ab9ef
Herr Erde <Natodamm@hotmail.com>
2025-04-03 13:43:10 +0200

    fix mailbox
    change from true to unix start time
    fix missing id

--- script/mailbox.py
+++ script/mailbox.py
@@ -2,31 +2,40 @@ import json
 
 mailbox_data_path = "temp/input/mailbox_data.json"
 output_file_path = "src/profile/ui_seen.json"
+collections_file = "temp/input/collections_data.json"
 
 try:
-    with open(output_file_path, "r+") as file:
-        try:
-            # Load existing data from ui_seen.json
-            data = json.load(file)
+    with open(collections_file, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        time_slot = data.get("timeSlot", "")
 
-            # Read entry names from mailbox_data.json
-            with open(mailbox_data_path, "r", encoding="utf-8") as mailbox_data_file:
-                mailbox_data = json.load(mailbox_data_file)
-                entry_names = mailbox_data.get("entries", [])
+    try:
+        with open(output_file_path, "r+", encoding="utf-8") as file:
+            try:
+                data = json.load(file)
 
-            # Replace the existing "localMailsSeen" dictionary with new entries
-            local_mails_seen = {entry_name: True for entry_name in entry_names}
+                with open(
+                    mailbox_data_path, "r", encoding="utf-8"
+                ) as mailbox_data_file:
+                    mailbox_data = json.load(mailbox_data_file)
+                    entry_names = mailbox_data.get("entries", [])
 
-            # Update the data with the new "localMailsSeen" dictionary
-            data["data"]["localMailsSeen"] = local_mails_seen
+                local_mails_seen = {
+                    entry_name: "1970-01-01T00:00:00Z" for entry_name in entry_names
+                }
 
-            # Write the updated data back to ui_seen.json
-            file.seek(0)
-            json.dump(data, file, indent=2)
-            file.truncate()
+                data["data"]["newSeasonPopupShownForID"] = time_slot
+                data["data"]["localMailsSeen"] = local_mails_seen
 
-        except (json.JSONDecodeError, KeyError) as e:
-            print(f"Error decoding JSON or key error in ui_seen.json: {e}")
+                file.seek(0)
+                json.dump(data, file, indent=2)
+                file.truncate()
+
+            except (json.JSONDecodeError, KeyError) as e:
+                print(f"Error decoding JSON or key error in ui_seen.json: {e}")
+
+    except IOError as e:
+        print(f"Error opening or accessing ui_seen.json: {e}")
 
 except IOError as e:
-    print(f"Error opening or accessing ui_seen.json: {e}")
+    print(f"Error opening or accessing collections_data.json: {e}")
